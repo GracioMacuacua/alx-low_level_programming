d342ddf50d708869a29a332d1627dbe27dbfe540
Grácio Macuácua <graciopdr@gmail.com>
2023-06-17 01:34:56 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /*
  *main - Entry Point
  *Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
