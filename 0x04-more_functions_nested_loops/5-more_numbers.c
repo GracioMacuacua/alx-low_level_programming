eb4b6274458d16f37f1a02d7064223765f6f7e96
Grácio Macuácua <graciopdr@gmail.com>
2023-06-23 05:12:02 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
 * more_numbers - print from 0 to 14 ten times.
-*
+* Return: void
 */
 
 void more_numbers(void)
@@ -12,9 +12,7 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-		{
 			_putchar(j);
-		}
 		_putchar('\n');
 	}
 	_putchar('\n');
