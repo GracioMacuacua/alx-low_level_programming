8798f066650231d2d90b5a5cb8c61bfd61fd42dc
Grácio Macuácua <graciopdr@gmail.com>
2023-06-26 21:49:07 +0200

    Updated advanced tasks from project 0x02

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
   * main - A program that print the sum even fibonacci numbers under 4 million
-  * Return: Nothing.
+  * Return: 0 (Success)
   */
 int main(void)
 {
@@ -9,14 +9,20 @@ int main(void)
 
 	num1 = total = 0;
 	num2 = 1;
+
 	for (fibonacci = 0; fibonacci < 50; fibonacci++)
 	{
 		num3 = num1 + num2;
 		num1 = num2;
 		num2 = num3;
+
 		if (num3 % 2 == 0 && num3 < 4000000)
-		total += num3;
+		{
+			total += num3;
+		}
 	}
+
 	printf("%lu\n", total);
+
 	return (0);
 }
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
   * main -A program that print the first 98 fibonacci numbers.
-  * Return: Nothing.
+  * Return: 0 (Success)
   */
 int main(void)
 {
