be4a51a4082a50f1d95f087ac89a5dcbe7f7754b
Grácio Macuácua <graciopdr@gmail.com>
2024-01-12 17:25:55 +0200

    Added the source files to the project 0x09 - Static libraries

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of a integer
+ * @n: The value to be transformed
+ * Return: The absolute value of the number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
