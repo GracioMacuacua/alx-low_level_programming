f8038fbffca9b8d5213de9321cb9950ae0243b0a
Grácio Macuácua <graciopdr@gmail.com>
2024-02-07 01:41:30 +0200

    Fixed some bugs

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,63 +1,44 @@
 #include "main.h"
 
 /**
- * _strlen - Looks for the length of a string
- * @s: A pointer to the string we want to know its length
- * Return: The length of the given string
- */
-int _strlen(char *s)
-{
-	int size;
-
-	size = 0;
-
-	while (s[size] != '\0')
-	{
-		size++;
-	}
-
-	return (size + 1);
-}
-
-/**
- * argstostr - concatenates all the arguments of your program.
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
  *
- * @ac: number of arguments.
- * @av: array of arguments;
- *
- * Return: a pointer to a string with all arguments.
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
  */
 char *argstostr(int ac, char **av)
 {
-	int numChars, i, j, k;
-	char *newString;
+	char *str;
+	int arg, byte, index, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	numChars = 0;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
 
-	for (i = 0; i < ac; i++)
-		numChars += _strlen(av[i]);
+	str = malloc(sizeof(char) * size + 1);
 
-	newString = (char *)malloc((sizeof(char) * numChars));
-
-	if (newString == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	k = 0;
+	index = 0;
 
-	for (i = 0; i < ac; i++)
+	for (arg = 0; arg < ac; arg++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			newString[k] = av[i][j];
-			k++;
-		}
-		newString[k] = '\n';
-		k++;
-	}
-	newString[k] = '\0';
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
 
-	return (newString);
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
 }
